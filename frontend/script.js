667bc482eadada04275de80fa84c285aa2f1fe4a
nquayson <31969365+nquayson@users.noreply.github.com>
2023-10-17 00:24:37 -0400

    add frontend

---
+++ frontend/script.js
@@ -0,0 +1,13 @@
+let mcount = ""
+url = "https://4pxyxy0e.lambda-url.us-east-1.on.aws/"
+fetch(url)
+  .then(response => response.text())
+  .then((response) => {
+      mcount = response
+      for (let i = 0; i < mcount.length; i++) {
+        var newSpan = document.createElement('span');
+        newSpan.innerHTML = mcount[i];
+        document.getElementById('mydiv').appendChild(newSpan);
+      }
+  })
+  .catch(err => console.log(err))
